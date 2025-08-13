5bb52598913cea9daa9b957a3244c3097748c8a8
Felipe Alvarado <devfelipe663@gmail.com>
2025-08-13 18:27:03 -0500

    feat: add list heroes

--- src/app/heroes/heroes.ts
+++ src/app/heroes/heroes.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: false,
   templateUrl: './heroes.html',
-  styleUrl: './heroes.css'
+  styleUrl: './heroes.css',
 })
 export class Heroes {
-  hero: Hero = {
-    id: 1,
-    name: 'Zeus'
-  };
+  heroes = HEROES;
 }

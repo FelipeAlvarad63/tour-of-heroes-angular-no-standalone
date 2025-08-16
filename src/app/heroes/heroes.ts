497ef0553f5d08d1bb1e6a7301df895b5429054e
Felipe Alvarado <devfelipe663@gmail.com>
2025-08-16 18:54:51 -0500

    feat: add event for show details to heroes

--- src/app/heroes/heroes.ts
+++ src/app/heroes/heroes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -9,4 +10,9 @@ import { HEROES } from '../mock-heroes';
 })
 export class Heroes {
   heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  };
 }
